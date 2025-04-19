ddc0377f90a76b7c1f32dd2cb702f8772428c453
Tolkun Omurbekova <tolkun3922@gmail.com>
2025-04-19 21:10:34 +0600

    -done homepage, added banners

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-	
-	
+    const swiperBanner = new Swiper('.swiper-banner', {
+        speed: 700,
+        spaceBetween: 0,
+        navigation:{
+            prevEl:'.swiper-banner .swiper-button-prev',
+            nextEl:'.swiper-banner .swiper-button-next'
+        },
+        pagination:{
+            el:'.swiper-banner .swiper-pagination',
+            clickable: true
+        }
+    });
 
 }, false);
