d4664aec3c195d491baa9008e70bb14aa8d4506b
Tolkun Omurbekova <tolkun3922@gmail.com>
2025-05-29 11:17:17 +0600

    -настроила адаптацию для блока отзывы - swiper

--- js/script.js
+++ js/script.js
@@ -1,36 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
     //header
-    $('.open-mobile-sub-menu').on('click', function (){
-       var mobilesubmenu = $(this).prev('.mobile-sub-menu');
-       if(mobilesubmenu.hasClass('is-active')){
-           mobilesubmenu.slideUp();
-           mobilesubmenu.removeClass('is-active');
-           $(this).removeClass('is-active');
-       }
-       else {
-           mobilesubmenu.slideDown();
-           mobilesubmenu.addClass('is-active');
-           $(this).addClass('is-active');
-       }
+    $('.open-mobile-sub-menu').on('click', function () {
+        var mobilesubmenu = $(this).prev('.mobile-sub-menu');
+        if (mobilesubmenu.hasClass('is-active')) {
+            mobilesubmenu.slideUp();
+            mobilesubmenu.removeClass('is-active');
+            $(this).removeClass('is-active');
+        } else {
+            mobilesubmenu.slideDown();
+            mobilesubmenu.addClass('is-active');
+            $(this).addClass('is-active');
+        }
     });
     //hamburger
     var headermobile = $('.header-mobile');
-    $('.header-hamburger').on('click', function (){
+    $('.header-hamburger').on('click', function () {
         headermobile.addClass('is-active');
     });
-    $('.header-mobile-close').on('click', function (){
+    $('.header-mobile-close').on('click', function () {
         headermobile.removeClass('is-active');
     });
     //banner
     const swiperBanner = new Swiper('.swiper-banner', {
         speed: 700,
         spaceBetween: 0,
-        navigation:{
-            prevEl:'.swiper-banner .swiper-button-prev',
-            nextEl:'.swiper-banner .swiper-button-next'
+        navigation: {
+            prevEl: '.swiper-banner .swiper-button-prev',
+            nextEl: '.swiper-banner .swiper-button-next'
         },
-        pagination:{
-            el:'.swiper-banner .swiper-pagination',
+        pagination: {
+            el: '.swiper-banner .swiper-pagination',
             clickable: true
         }
     });
@@ -43,10 +42,10 @@ document.addEventListener('DOMContentLoaded', function () {
         // use element for option
         columnWidth: '.grid-sizer',
         percentPosition: true,
-        gutter:20,
+        gutter: 20,
     });
     // layout Masonry after each image loads
-    $grid.imagesLoaded().progress( function() {
+    $grid.imagesLoaded().progress(function () {
         $grid.masonry('layout');
     });
 
@@ -62,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // The "opener" function should return the element from which popup will be zoomed in
             // and to which popup will be scaled down
             // By defailt it looks for an image tag:
-            opener: function(openerElement) {
+            opener: function (openerElement) {
                 // openerElement is the element on which popup was initialized, in this case its <a> tag
                 // you don't need to add "opener" option if this code matches your needs, it's defailt one.
                 return openerElement.is('img') ? openerElement : openerElement.find('img');
@@ -73,17 +72,16 @@ document.addEventListener('DOMContentLoaded', function () {
     //gallery END
 
     //faq START
-    $('.faq-question').on('click',function (){
+    $('.faq-question').on('click', function () {
         var answer = $(this).next('.faq-answer');
-       if ($(this).hasClass('is-active')){
-           answer.slideUp();
-           $(this).removeClass('is-active');
-       }
-       else {
-           answer.slideDown();
-           $(this).addClass('is-active');
+        if ($(this).hasClass('is-active')) {
+            answer.slideUp();
+            $(this).removeClass('is-active');
+        } else {
+            answer.slideDown();
+            $(this).addClass('is-active');
 
-       }
+        }
     })
     //faq END
 
@@ -91,10 +89,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const swiperReviews = new Swiper('.swiper-reviews', {
         speed: 700,
         spaceBetween: 20,
-        slidesPerView:4,
-        pagination:{
-            el:'.swiper-reviews .swiper-pagination',
+        pagination: {
+            el: '.swiper-reviews .swiper-pagination',
             clickable: true
+        },
+        breakpoints: {
+            '480': {
+                slidesPerView: 2,
+            },
+            '768': {
+                slidesPerView: 3,
+            },
+            '1024': {
+                slidesPerView: 4,
+            },
         }
     });
 }, false);
